d615a2c617c69fc41919492ee9143909feda2368
phcaradanai <phcaradanai@gmail.com>
2024-06-24 16:37:14 +0700

    add qr

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Hello! I,am Phatcharadanai Kanyapan</p>
+      <Image
+        alt="qr_code"
+        src="/images/PCV.png"
+        width={200}
+        height={200}
+      ></Image>
       <button className="glitch-button">
         <a href="https://drive.google.com/uc?export=download&id=1NSe2ljkxIFPCkPvw1ogfLwKHqEeL5hiS">
           DOWLOAD CV
